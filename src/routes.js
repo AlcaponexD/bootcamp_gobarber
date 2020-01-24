0afa5999a0b49fafd6e3bf17b114d91b1a8140ea
Jeison Pedroso <jeison.contas@gmail.com>
2020-01-24 07:59:21 -0300

    iniciar sessão

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
 import User from './app/models/User'
-
 import UserController from './app/controllers/User';
+import SessionController from './app/controllers/Session';
 
 const routes = new Router();
 
 routes.post('/users',UserController.store);
-
+routes.post('/sessions',SessionController.store);
 
 export default routes;
